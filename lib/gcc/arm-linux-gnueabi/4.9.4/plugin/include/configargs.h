8609151de830c6b9a65fec96f4e95cb50bf94249
Rajat Gupta <rajatgupta1998@acm.org>
2019-06-19 10:57:47 +0000

    gcc-4.x: update and init from GNU GCC 4.9.4
    Signed-off-by: Rajat Gupta <rajatgupta1998@acm.org>

---
+++ lib/gcc/arm-linux-gnueabi/4.9.4/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc/configure --disable-multilib --disable-werror --enable-languages=c,c++ --target=arm-linux-gnueabi --prefix=/home/rajatgupta1998/android/aosp/build-tools-gcc/arm-linux-gnueabi";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "tls", "gnu" } };
